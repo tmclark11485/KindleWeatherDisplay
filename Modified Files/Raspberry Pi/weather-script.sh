7ac081bbf4c53522b44fb7790837d7b5aa82e4c1
tmclark11485 <53068738+tmclark11485@users.noreply.github.com>
2022-01-30 19:55:32 -0600

    Changed to use rsvg-convert, also changed file path to look start in home directory

--- Modified Files/Raspberry Pi/weather-script.sh
+++ Modified Files/Raspberry Pi/weather-script.sh
@@ -5,4 +5,4 @@ cd "$(dirname "$0")"
 python3.9 weather-script.py
 rsvg-convert --background-color=white -o weather-script-output.png weather-script-output.svg
 pngcrush -c 0 -nofilecheck weather-script-output-new.png weather-script-output.png
-cp -f weather-script-output.png /weatherserver/weather-script-output-new.png
+cp -f weather-script-output.png ~/weatherserver/weather-script-output-new.png
